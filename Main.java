c9074a3119729f9b0f37deef86db6c578cfe2e60
Ravuri25 <ravurivenky0@gmail.com>
2025-06-01 23:13:12 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+ public static void main(String[] args)
+{
+int rollno = 1596;
+String sname = "RVR";
+String course = "C";
+System.out.println("My name is :  "+ sname);
+System.out.println("My Course is :  "+ course);
+System.out.println("My Roll number is :  "+ rollno);
+System.out.println("GOOD EVENING");
+}
+}
